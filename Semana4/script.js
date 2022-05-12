212d61976f79374f81ddda60ad1f1b1352e1b63b
thainadedeus <99210798+thainadedeus@users.noreply.github.com>
2022-05-12 15:33:33 -0300

    Usar javascript no curriculo
    Utilização de javascript no código

--- Semana4/script.js
+++ Semana4/script.js
@@ -1,15 +1,5 @@
 //comentário em js
-function ex1(){
-//criando uma mensagem de aviso com alert
-alert('SOS');
-
-var v0 = document.getElementById('v0').value;
-const g = 10; //aceleração gravitacional 10m/s^2
-var h = v0*v0 / (2 * g)
-
-tSubida = v0/g;
-
-document.getElementById('vem').innerHTML = "TEMPO DE SUBIDA = " + tSubida;
-document.getElementById('vem').innerHTML += "<br>Altura máxima = " + h + "m";
-  
+function linha(){
+//$('vem').html("Acrescentar");
+document.getElementById('vem').innerHTML += document.getElementById('texto').value;
 }
