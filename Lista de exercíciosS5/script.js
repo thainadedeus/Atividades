f79725b3b2d234c083c64f85adf92fde7c9f3d38
thainadedeus <99210798+thainadedeus@users.noreply.github.com>
2022-05-22 20:56:26 -0300

    lista de exercíco

---
+++ Lista de exercíciosS5/script.js
@@ -0,0 +1,6 @@
+function opera() {
+    var primeiro  = document.getElementById('primeiro').value;
+    var segundo   = document.getElementById('segundo').value;
+    var operacoes = document.getElementById('operacoes').value;
+    var resultado;
+}
