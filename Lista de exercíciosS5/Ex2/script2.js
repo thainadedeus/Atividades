b147be7245d397768cf7c3ecaf81add78c7c53f0
thainadedeus <99210798+thainadedeus@users.noreply.github.com>
2022-06-02 15:22:16 -0300

    Currículo 6 e 7

--- Lista de exercíciosS5/Ex2/script2.js
+++ Lista de exercíciosS5/Ex2/script2.js
@@ -1,13 +1,14 @@
 function opera() {
     var produto = document.getElementById('produto').value;
     var preco = document.getElementById('preco').value;
-    var nota100,nota50;
+    var nota100,nota50,nota20,nota10,nota5,nota2,nota1;
     var nota100 = parseInt(document.getElementById('preco').value / 100);
     document.getElementById('chama').innerHTML += nota100 + "notas de 100<br>";
 
     var resto100 = parseInt(document.getElementById('preco').value % 100);
     console.log(resto100);
-    if(parseInt(resto100 / 50 )>=50)
+    if(parseInt(resto100 / 50 )>=50);
+    if(((parseInt(resto100 / 50 )>=50)>=20)/20);
     var nota50 = parseInt(resto100 / 50 );
     document.getElementById('chama').innerHTML += nota50;
 }
