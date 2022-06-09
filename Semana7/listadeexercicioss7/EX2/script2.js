b3947a910028ef341dbd50b990384592e3d7a235
thainadedeus <99210798+thainadedeus@users.noreply.github.com>
2022-06-09 15:21:54 -0300

    Update script2.js

--- Semana7/listadeexercicioss7/EX2/script2.js
+++ Semana7/listadeexercicioss7/EX2/script2.js
@@ -1,6 +1,8 @@
 function trocou() {
     var prim = document.getElementById('pri').value;
     var segu = document.getElementById('seg').value;
+    document.getElementById('pri').value = segu;
+    document.getElementById('seg').value = prim;
    var myArray = [ prim, segu];
    myArray.reverse();
 document.getElementById('vem').innerhtml += myArray;
